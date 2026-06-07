23e7429679ab42ae722db28320c64959cf249c9d
Ahmad Alshara <ahm3230369@ju.edu.jo>
2026-06-07 22:40:39 +0300

    quick syntax fix

--- Frontend/src/pages/Auth/Login.jsx
+++ Frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import auth from '../../services/auth'
 import { Link, useNavigate } from 'react-router-dom'
+import auth from '../../services/auth'
 
 export default function Login() {
   const navigate = useNavigate()
